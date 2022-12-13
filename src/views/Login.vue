1102cf179d250675e383d9e15ccb876ff8f0fa13
AkumuJey <josephowino7@gmail.com>
2022-12-14 00:26:48 +0300

    Footer and Sign Up modifications

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,32 @@
 <template>
     <div class="grid h-full w-full bg-white">
-      <div class="w-inline-grid sm:w-1/2 mx-auto rounded-3xl text-sm mt-5">
-        <form id="signin-form" @submit.prevent="signInRequest" class="bg-blue-200 p-5 rounded-xl">
-          <label for="email" class="">Email:</label>
-          <input type="email" v-model="email" name="email" id="email" class="w-full rounded-xl shadow-2xl focus:outline-none p-2">
-          <label for="password" class="">Password:</label>
-          <input type="password" v-model="password" name="password" id="password" class="w-full rounded-xl shadow-2xl focus:outline-none p-2">
-          <p v-if="errMsg" class="text-red-700">{{ errMsg }}</p>
-          <button type="submit" class="bg-blue-800 hover:bg-blue-600 p-2 w-full my-4 rounded-xl text-white">Login</button>
-          <div class="grid grid-flow-col text-white">
-            <router-link  class="p-2 text-center block bg-blue-800 hover:bg-blue-600 rounded-xl text-white" :to="{name : 'signUp'}">Register</router-link>
-          </div>
-        </form>  
+      
+      <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 mx-auto mb-5 dark:bg-gray-800 dark:border-gray-700">
+        <form class="space-y-6" @submit.prevent="signInRequest">
+            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+            <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="example@email.com" required>
+            </div>
+            <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                <p v-if="errMsg" class="text-red-700">{{ errMsg }}</p>
+            </div>
+            <div class="flex items-start">
+                <div class="flex items-start">
+                    <div class="flex items-center h-5">
+                        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                    </div>
+                    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+                <a href="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+            </div>
+            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+            <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+                Not registered? <router-link :to="{name : 'signUp'}" class="text-blue-700 hover:underline dark:text-blue-500">Create account</router-link>
+            </div>
+        </form>
       </div>
     </div>
 </template>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -7,9 +7,14 @@
           <label for="password" class="">Enter Password:</label>
           <input v-model="password" type="password" name="password" id="password" class="w-full rounded-xl shadow-2xl focus:outline-none p-2">
           <button type="submit" class="bg-blue-800 hover:bg-blue-600 p-2 w-full my-4 rounded-xl text-white">Register</button>
-          <button @click="signInWithGoogle">Sign In With Google</button>
+          <div class="flex place-content-center justify-center">
+            <button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"  @click="signInWithGoogle">
+              <svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
+              Sign in with Google
+            </button>
+          </div>
           <div class="">
-            <router-link :to="{ name: 'login' }" class="p-2 text-center text-white block bg-blue-800 hover:bg-blue-600 rounded-xl">Sign in</router-link>
+            <router-link :to="{ name: 'login' }" class="p-2 text-center text-white block bg-blue-800 hover:bg-blue-600 rounded-xl">Already have an account?</router-link>
           </div>
         </form>  
       </div>
