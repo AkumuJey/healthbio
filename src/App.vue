2e4121e6e25352a3b622bbe572288824f78f017b
AkumuJey <josephowino7@gmail.com>
2022-12-19 12:57:22 +0300

    Redesign of navbar

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,33 @@
 <template>
-    <div class="bg-red-300 dark:bg-gray-900 mb-10">
-      <nav class="top-0 left-0 z-20 p-4 w-full bg-white border-gray-200 px-2 md:px-4 py-2.5 dark:bg-gray-900 h-auto">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Jey's Hospital</span>
+    <div class="">
+      <div>
+        <nav class="flex justify-between">
+          <div>
+            <a href="#">
+              <span>
+                Jey's Hospital
+              </span>
             </a>
-            <div class="flex items-center md:order-2">
-                <router-link :to="{ name: 'login' }" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800" v-if="!isLoggedIn">Login</router-link>
-                <button class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800" v-if="isLoggedIn" @click="handleSignOut">Sign out</button>
-                <router-link :to="{ name: 'signUp' }" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</router-link>
-                <button data-collapse-toggle="mega-menu-icons" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-icons" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                </button>
+          </div>
+          <div class="flex sm:justify-around sm:gap-6">
+            <div>
+              <ul class="sm:flex sm:justify-center">
+                <li><router-link :to="{ name: 'home' }" class="">Home</router-link></li>
+                <li><router-link :to="{ name: 'about' }" class="">About</router-link></li>
+              </ul>
             </div>
-            <div id="mega-menu-icons" class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
-                <ul class="flex flex-col mt-4 text-sm font-medium md:flex-row md:space-x-8 md:mt-0">
-                    <li>
-                        <router-link :to="{ name: 'home' }" class="block py-2 pr-4 pl-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</router-link>
-                    </li>
-                    <router-link :to="{ name: 'about' }" class="flex items-center text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 group">
-                        <span class="sr-only">About us</span>
-                        <svg class="mr-2 w-4 h-4 text-gray-400 dark:text-gray-500 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" focusable="false" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-                        About Us
-                    </router-link>
-                </ul>
+            <div>
+              <button v-if="!isLoggedIn" class="btn"><router-link :to="{ name: 'login' }" class="" v-if="!isLoggedIn">Login</router-link></button>
+              <button
+              v-else class="btn"
+              @click="handleSignOut">Sign Out</button>
             </div>
-        </div>
-      </nav>
-
-      <div class="my-5">
+          </div>
+        </nav>
+      </div>
+      <div class="">
         <router-view/>
       </div>
-            
-      <footer class="bottom-0 left-0 z-20 p-4 w-full bg-slate-100 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
-            </li>
-        </ul>
-      </footer>
     </div>
 </template>
 
@@ -86,6 +62,5 @@ const handleSignOut = () => {
 </script>
 
 <style>
-
 
 </style>
