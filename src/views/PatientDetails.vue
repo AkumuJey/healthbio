b2975c073dcbc180abb1623a0b5bb475699e393a
AkumuJey <josephowino7@gmail.com>
2022-12-23 15:32:05 +0300

    Dynamic tab sytem with compoition api

--- src/views/PatientDetails.vue
+++ src/views/PatientDetails.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button v-for="tab in tabs" :key="tab" @click="component = tab" class="bg-slate-300 p-2 mx-2 rounded-xl active:bg-blue-600">
+        <button v-for="tab in tabs" :key="tab" @click="setComponent(tab)" class="bg-slate-300 p-2 mx-2 rounded-xl active:bg-blue-600">
             {{ tab }}
         </button>
         <keep-alive>
@@ -9,8 +9,54 @@
     </div>
   </template>
   
-
   <script>
+  import { ref, computed, defineComponent } from 'vue'
+  import Vitals from '@/components/patientDetails/Vitals.vue';
+  import AdditionalHistory from '@/components/patientDetails/AdditionalHistory.vue';
+  import Biodata from '@/components/patientDetails/Biodata.vue';
+  import Complaints from '@/components/patientDetails/Complaints.vue';
+  import TreatmentPlan from '@/components/patientDetails/TreatmentPlan.vue';
+  
+  export default {
+    components: { 
+        AdditionalHistory,
+        Vitals,
+        TreatmentPlan,
+        Biodata,
+        Complaints
+    },
+    setup() {
+      const tabs = [
+        'Biodata',
+        'Complaints',
+        'Vitals',
+        'AdditionalHistory',
+        'TreatmentPlan'
+      ]
+      const component = ref('Biodata')
+  
+      const setComponent = (value) => {
+        component.value = value
+      }
+  
+      return {
+        tabs,
+        component,
+        setComponent
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+
+
+
+
+
+  <!-- <script>
     import AdditionalHistory from '@/components/patientDetails/AdditionalHistory.vue';
     import Vitals from '@/components/patientDetails/Vitals.vue';
     import TreatmentPlan from '@/components/patientDetails/TreatmentPlan.vue';
@@ -39,8 +85,12 @@ export default {
         }
     }
 }
-  </script>
-  
-  <style>
-  
-  </style>
+  </script> -->
+  <!-- <template>
+    <div>
+      <button v-for="tab in tabs" :key="tab" @click="setComponent(tab)">
+        {{tab}}
+      </button>
+      <component :is="component" />
+    </div>
+  </template> -->
