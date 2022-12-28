3f1809784d88621f04b608affa97b93c2349f20d
AkumuJey <josephowino7@gmail.com>
2022-12-28 23:02:25 +0300

    Modified navbar

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@
       <div class="sticky top-0 bg-slate-300 shadow-xl px-5 py-2">
         <nav class="flex justify-between">
           <div class="flex flex-nowrap gap-4">
-              <button @click="toggleMenu" class="sm:hidden">
-               <span>
-                menu
-               </span>
-              </button>
             <a href="#">
               <span>
                 Jey's Hospital
               </span>
             </a>
           </div>
-          <div class="flex sm:justify-around justify-between sm:gap-6 w-[60%]">
+          <div class="flex flex-row-reverse md:flex-row md:justify-between gap-6 w-[60%]">
             <div>
-              <ul id="menuIcon" :class="{'hidden': menu}" class="absolute px-5 py-2 rounded-2xl sm:gap-4 sm:static sm:h-auto top-12 mx-auto left-8 bg-slate-200 sm:flex
-               sm:justify-center dark:bg-slate-400">
+              <button @click="toggleMenu" class="md:hidden justify-end">
+                <span>
+                 menu
+                </span>
+               </button>
+              <ul id="menuIcon" :class="{'hidden': menu}" class="absolute px-5 py-2 rounded-2xl md:gap-4 md:static md:h-auto h-screen w-full top-[100%] mx-auto left-0 bg-slate-200 md:flex
+               md:justify-center dark:bg-slate-400">
                 <li><router-link :to="{ name: 'home' }" class="">Home</router-link></li>
                 <li><router-link :to="{ name: 'patientDetails' }" class="">Patient Details</router-link></li>
                 <li><router-link :to="{ name: 'about' }" class="">About</router-link></li>
@@ -39,10 +39,11 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue"
+import { defineComponent, onMounted, ref } from "vue"
 
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import router from "./router";
+
 
 const isLoggedIn = ref(false)
 const menu = ref(true)
---
+++ src/components/patientDetails/navbar/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="flex justify-between">
+        <div class="flex flex-nowrap gap-4">
+          <a href="#">
+            <span>
+              Jey's Hospital
+            </span>
+          </a>
+        </div>
+        <div class="flex flex-row-reverse md:flex-row md:justify-between gap-6 w-[60%]">
+          <div>
+            <button @click="toggleMenu" class="md:hidden justify-end">
+              <span>
+               menu
+              </span>
+             </button>
+            <ul id="menuIcon" :class="{'hidden': menu}" class="absolute px-5 py-2 rounded-2xl md:gap-4 md:static md:h-auto top-12 mx-auto left-8 bg-slate-200 md:flex
+             md:justify-center dark:bg-slate-400">
+              <li><router-link :to="{ name: 'home' }" class="">Home</router-link></li>
+              <li><router-link :to="{ name: 'patientDetails' }" class="">Patient Details</router-link></li>
+              <li><router-link :to="{ name: 'about' }" class="">About</router-link></li>
+            </ul>
+          </div>
+          <div>
+            <button v-if="!isLoggedIn" class="btn"><router-link :to="{ name: 'login' }" class="" v-if="!isLoggedIn">Login</router-link></button>
+            <button
+            v-else class="btn"
+            @click="handleSignOut">Sign Out</button>
+          </div>
+        </div>
+      </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
