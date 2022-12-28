b790cbb7038784f5b47c14700b4655fce9d503e3
AkumuJey <josephowino7@gmail.com>
2022-12-29 00:28:44 +0300

    Mobile fiendly navbar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,7 @@
 <template>
     <div class="">
-      <div class="sticky top-0 bg-slate-300 shadow-xl px-5 py-2">
-        <nav class="flex justify-between">
-          <div class="flex flex-nowrap gap-4">
-            <a href="#">
-              <span>
-                Jey's Hospital
-              </span>
-            </a>
-          </div>
-          <div class="flex flex-row-reverse md:flex-row md:justify-between gap-6 w-[60%]">
-            <div>
-              <button @click="toggleMenu" class="md:hidden justify-end">
-                <span>
-                 menu
-                </span>
-               </button>
-              <ul id="menuIcon" :class="{'hidden': menu}" class="absolute px-5 py-2 rounded-2xl md:gap-4 md:static md:h-auto h-screen w-full top-[100%] mx-auto left-0 bg-slate-200 md:flex
-               md:justify-center dark:bg-slate-400">
-                <li><router-link :to="{ name: 'home' }" class="">Home</router-link></li>
-                <li><router-link :to="{ name: 'patientDetails' }" class="">Patient Details</router-link></li>
-                <li><router-link :to="{ name: 'about' }" class="">About</router-link></li>
-              </ul>
-            </div>
-            <div>
-              <button v-if="!isLoggedIn" class="btn"><router-link :to="{ name: 'login' }" class="" v-if="!isLoggedIn">Login</router-link></button>
-              <button
-              v-else class="btn"
-              @click="handleSignOut">Sign Out</button>
-            </div>
-          </div>
-        </nav>
+      <div class="">
+       <Navbar class="z-[5]"/>
       </div>
       <div class="">
         <router-view/>
@@ -39,36 +10,8 @@
 </template>
 
 <script setup>
-import { defineComponent, onMounted, ref } from "vue"
 
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import router from "./router";
-
-
-const isLoggedIn = ref(false)
-const menu = ref(true)
-
-let auth;
-onMounted(() => {
-  auth = getAuth()
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      isLoggedIn.value = true
-    }
-    else {
-      isLoggedIn.value = false
-    }
-  })
-})
-
-const handleSignOut = () => {
-  signOut(auth).then(() => {
-    router.push('/login')
-  })
-}
-const toggleMenu = () => {
-  menu.value = !menu.value
-}
+import Navbar from '@/components/patientDetails/navbar/Navbar.vue'
 
 </script>
 
--- src/components/patientDetails/navbar/Navbar.vue
+++ src/components/patientDetails/navbar/Navbar.vue
@@ -1,37 +1,79 @@
 <template>
-    <nav class="flex justify-between">
-        <div class="flex flex-nowrap gap-4">
-          <a href="#">
-            <span>
-              Jey's Hospital
-            </span>
-          </a>
-        </div>
-        <div class="flex flex-row-reverse md:flex-row md:justify-between gap-6 w-[60%]">
-          <div>
-            <button @click="toggleMenu" class="md:hidden justify-end">
-              <span>
-               menu
-              </span>
-             </button>
-            <ul id="menuIcon" :class="{'hidden': menu}" class="absolute px-5 py-2 rounded-2xl md:gap-4 md:static md:h-auto top-12 mx-auto left-8 bg-slate-200 md:flex
-             md:justify-center dark:bg-slate-400">
-              <li><router-link :to="{ name: 'home' }" class="">Home</router-link></li>
-              <li><router-link :to="{ name: 'patientDetails' }" class="">Patient Details</router-link></li>
-              <li><router-link :to="{ name: 'about' }" class="">About</router-link></li>
-            </ul>
-          </div>
-          <div>
-            <button v-if="!isLoggedIn" class="btn"><router-link :to="{ name: 'login' }" class="" v-if="!isLoggedIn">Login</router-link></button>
-            <button
-            v-else class="btn"
-            @click="handleSignOut">Sign Out</button>
-          </div>
-        </div>
-      </nav>
+  <nav class="flex justify-between sticky top-0 bg-slate-300 shadow-xl px-5 py-2">
+    <div class="flex flex-nowrap gap-4">
+      <a href="#">
+        <span>
+          Jey's Hospital
+        </span>
+      </a>
+    </div>
+    <div class="flex flex-row-reverse md:flex-row md:justify-between gap-6 w-[60%]">
+      <div>
+        <button @click="toggleMenu" class="md:hidden justify-end">
+          <span>
+           menu
+          </span>
+         </button>
+        <ul id="menuIcon" :class="{'hidden': menu}" class="absolute px-5 py-2 md:rounded-2xl
+         md:gap-4 md:static md:h-auto h-screen 
+         w-full top-[100%] mx-auto left-0 bg-slate-200 md:flex
+         md:justify-center dark:bg-slate-400">
+          <li><router-link
+            :to="{ name: 'home' }" class="">Home
+          </router-link></li>
+          <li><router-link
+            :to="{ name: 'patientDetails' }" class="">Patient Details
+          </router-link></li>
+          <li><router-link
+            :to="{ name: 'about' }" class="">About
+          </router-link></li>
+        </ul>
+      </div>
+      <div>
+        <button v-if="!isLoggedIn" class="btn"
+        ><router-link :to="{ name: 'login' }" class="" v-if="!isLoggedIn"
+        >Login
+        </router-link></button>
+
+        <button
+        v-else class="btn"
+        @click="handleSignOut">Sign Out
+        </button>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue"
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import router from "../../../router";
+const isLoggedIn = ref(false)
+const menu = ref(true)
+
+let auth;
+onMounted(() => {
+  auth = getAuth()
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true
+    }
+    else {
+      isLoggedIn.value = false
+    }
+  })
+})
+
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    router.push('/login')
+  })
+}
+const toggleMenu = () => {
+  menu.value = !menu.value
+}
+
+
 
 </script>
 
