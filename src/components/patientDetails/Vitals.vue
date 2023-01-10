4ef67f1e5f337d197822d8f003ee6072c3292c11
AkumuJey <josephowino7@gmail.com>
2023-01-10 08:11:02 +0300

    Typing watch worked

--- src/components/patientDetails/Vitals.vue
+++ src/components/patientDetails/Vitals.vue
@@ -3,14 +3,20 @@
       <div>
           <h1>Vitals</h1>
           <button @click="num++" class="p-2 bg-blue-500 mx-auto block">{{ num }}</button>
+          <p v-if="typing">Typing...</p>
       </div>
     </div>
   </template>
   
   <script setup>
-  import { ref } from "vue";
+  import { ref, watch } from "vue";
   
   const num = ref(0)
+  const typing = ref(false)
+ 
+  watch(num, () => typing.value = true)
+
+ 
   </script>
   
   <style>
